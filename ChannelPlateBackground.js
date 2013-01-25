577f183b01a7ba9969f2bc31a11e502cfe67b42d
johnjbarton <johnjbarton@johnjbarton.com>
2013-01-25 14:37:53 -0800

    log request counts if DEBUG

--- ChannelPlateBackground.js
+++ ChannelPlateBackground.js
@@ -9,23 +9,36 @@
 
 var DEBUG = false;
 
+var totalRequests = 0;
+var totalCallback = 0;
+var totalErrback = 0;
+
+if (DEBUG) {
+  setInterval(function() {
+    console.log("Requests " + totalRequests + " = " + totalCallback + ' callbacks + ' + totalErrback + ' errbacks');
+  }, 5000);
+}
+
 var ChromeBackgroundMethods = {};
 
 // Cross site XHR, xhr(url) -> content 
 //
 ChromeBackgroundMethods.request = function(method, url, callback, errback) {
+  if (!callback || !errback) {
+    throw new Error("Both callback and errback functions are required");
+  }
   var xhr = new XMLHttpRequest();
   xhr.open(method, url);
   xhr.addEventListener('load', function(e) {
     if (xhr.status == 200 || xhr.status == 0) {
       if (DEBUG) 
         console.log("end xhr "+url);
-      
+      totalCallback++;
       callback(xhr.responseText);
     } else {
       if (DEBUG) 
         console.error("err xhr "+url);
-
+      totalErrback++;
       errback(xhr.status);
     }
   }.bind(this), false);
@@ -34,6 +47,7 @@ ChromeBackgroundMethods.request = function(method, url, callback, errback) {
   }.bind(this);
   xhr.addEventListener('error', onFailure, false);
   xhr.addEventListener('abort', onFailure, false);
+  totalRequests++;
   xhr.send();
 };
 
